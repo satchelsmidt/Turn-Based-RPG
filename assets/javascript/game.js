1f6edf5916aec79c70c7cb0dd584aac44ea641c6
supersatchel <34499356+satchelsmidt@users.noreply.github.com>
2019-07-17 20:18:43 -0700

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,127 @@
 //PSEUDOCODE -- turn based RPG
 // Begin with four images on screen with names and health amounts for each 
 //Player clicks on any one of the images
-//The imagae that the player clicked on becomes their main character (moves to 'your character' section), the other three images become 'enemies' (move to 'enemies available' section) 
+//The image that the player clicked on becomes their main character (moves to 'your character' section), the other three images become 'enemies' (move to 'enemies available' section) 
 
-$(document).ready(function() {
+//If first card selected...
 
-    // Here we use jQuery to select the header with "click-me" as its ID.
-    // Notice I have the #click-me, click, and then the function
-    // So $("#id|.class|element").on("action", function(){})
+var yourCharacter;
+var enemy_1;
+var enemy_2;
+var enemy_3;
+
+var character1Selected = false;
+var character2Selected = false;
+var character3Selected = false;
+var character4Selected = false;
+
+if(character1Selected === false){
+    $(document).ready(function() {
+        $("#card_1").on("click", function() {
+        // console.log("bitch ass")
+        alert("Let the battle begin!");
+
+        $(".content").css("position", "absolute");
+        $(".content").css("top", "5px");
+        character1Selected = true;
     
-    $("#card_1").on("click", function() {
-      alert("Let the battle begin!");
 
-      var yourCharacter = $("#card_1");
-      $("#character").append(yourCharacter);
+        yourCharacter = $("#card_1");
+        $("#character").append(yourCharacter);
 
-      var enemy_1 = $("#card_2");
-      $("#enemies").append(enemy_1);
+        enemy_1 = $("#card_2");
+        $("#enemies").append(enemy_1);
 
-      var enemy_2 = $("#card_3");
-      $("#enemies").append(enemy_2);
+        enemy_2 = $("#card_3");
+        $("#enemies").append(enemy_2);
 
-      var enemy_3 = $("#card_4");
-      $("#enemies").append(enemy_3);
+        enemy_3 = $("#card_4");
+        $("#enemies").append(enemy_3);
+        });
     });
-});
+}
+ 
+if(character2Selected === false){
+//If second card selected...
+    $(document).ready(function() {
+        $("#card_2").on("click", function() {
+        alert("Let the battle begin!");
 
-//Player can then click on one of the remaining images as the one to attack, selected image is moved below player's icon
+        $(".content").css("position", "absolute");
+        $(".content").css("top", "5px");
+        character2Selected = true;
+
+        yourCharacter = $("#card_2");
+        $("#character").append(yourCharacter);
+
+        enemy_1 = $("#card_1");
+        $("#enemies").append(enemy_1);
+
+        enemy_2 = $("#card_3");
+        $("#enemies").append(enemy_2);
+
+        enemy_3 = $("#card_4");
+        $("#enemies").append(enemy_3);
+        });
+    });
+ }
+
+ if(character3Selected === false){
+    //If third card selected...
+    $(document).ready(function() {
+        $("#card_3").on("click", function() {
+        alert("Let the battle begin!");
+
+        $(".content").css("position", "absolute");
+        $(".content").css("top", "5px");
+        character3Selected = true;
+
+        yourCharacter = $("#card_3");
+        $("#character").append(yourCharacter);
+
+        enemy_1 = $("#card_1");
+        $("#enemies").append(enemy_1);
+
+        enemy_2 = $("#card_2");
+        $("#enemies").append(enemy_2);
+
+        enemy_3 = $("#card_4");
+        $("#enemies").append(enemy_3);
+        });
+    });
+}
+
+if(character4Selected === false){
+    //If fourth card selected...
+    $(document).ready(function() {
+        $("#card_4").on("click", function() {
+        alert("Let the battle begin!");
+
+        $(".content").css("position", "absolute");
+        $(".content").css("top", "5px");
+        character4Selected = true;
+
+        yourCharacter = $("#card_4");
+        $("#character").append(yourCharacter);
+
+        enemy_1 = $("#card_1");
+        $("#enemies").append(enemy_1);
+
+        enemy_2 = $("#card_2");
+        $("#enemies").append(enemy_2);
+
+        enemy_3 = $("#card_3");
+        $("#enemies").append(enemy_3);
+        });
+    });
+}
+
+//Player can then click on one of the 'enemy' images as the one to attack, selected image is moved to current opponent section
+
+$(enemy_1).on("click", function(){
+    alert("WAAAAAAAAAAAAAAA");
+})
+
 //Player can then click 'attack' button, which reduces enemy health. Each player click of attack results in enemy losing health, and then player loses health as enemy strikes back w/ random damage numbers
 // Once player health under 0, game is over and you are defeated, restart button appears
 // Player does more damage with each attack button press
